9560cabcc99971a9a4c3e09325100f1903371ea2
Michael Le <michle@microsoft.com>
2021-03-24 14:06:41 -0700

    Add project files.

---
+++ Chat.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Chat_Room_Tutorial
+{
+    public class Chat : Hub
+    {
+        public void BroadcastMessage(string name, string message)
+        {
+            Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+
+        public void Echo(string name, string message)
+        {
+            Clients.Client(Context.ConnectionId).SendAsync("echo", name, message + " (echo from server)");
+        }
+    }
+}
