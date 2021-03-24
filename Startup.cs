97c4b0bc7f44cb5191ec3881f4092e9046475e77
Michael Le <michle@microsoft.com>
2021-03-24 14:12:35 -0700

    Modify startup to use signal R and the chat hub in URL.

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +12,7 @@ namespace Chat_Room_Tutorial
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +27,8 @@ namespace Chat_Room_Tutorial
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Use the /chat endpoint so clients can access the hub using this URL.
+                endpoints.MapHub<Chat>("/chat");
             });
         }
     }
